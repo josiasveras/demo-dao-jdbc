7b5d9eb06c80b720230a5047a114a3992d030b2d
josiasveras <josias_veras@hotmail.com>
2021-08-02 11:52:50 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n------ TEST #4: seller findAll ------");
+		System.out.println("\n------ TEST #4: seller insert ------");
 		Seller newSeller = new Seller(null, "Greg Orange", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n------ TEST #5: seller update ------");
+		seller = sellerDao.findById(1);
+		seller.setName("Bob Brown Junior");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
